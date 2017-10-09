12bfbe2194b6551ce8550468b45a7a98b4792b63
Rahul S <Rahulsidhpura912@gmail.com>
2017-10-10 01:31:50 +0530

    My Project Setup Completed Successfully

---
+++ ttpbackend/src/main/java/net/rah/ttpbackend/App.java
@@ -0,0 +1,13 @@
+package net.rah.ttpbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
